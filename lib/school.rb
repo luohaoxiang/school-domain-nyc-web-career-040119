26157e0d0b2fa535e6f4f864a13d28fa3d6e5e2d
Adam Robinson <luohaoxiang@yahoo.com>
2019-04-23 18:54:28 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade].map do |student|
+      student
+    end
+  end
+
+  def sort
+    @roster.each do |key, val|
+      @roster[key] = val.sort
+    end
+  end
+
+end
